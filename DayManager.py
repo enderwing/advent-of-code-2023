9149fc0481656012a841de176bbd651a674ba844
enderwing <enderwing1@gmail.com>
2023-12-17 23:41:32 -0500

    refactored DayManager.py to be cooler - stores the example input for each part of each day separately - can go back after importing a day to import another day - selecting a missing day will ask if you want to create it instead - code refactor - added test input files for previous days

--- DayManager.py
+++ DayManager.py
@@ -1,76 +1,126 @@
-import importlib as imp
+import importlib
 import importlib.util
+import sys
 
 
+def getInput(prompt):
+    global userInput
+    print(prompt)
+    userInput = input(">> ").strip().lower()
+
+def process_input():
+    global curPrompt, userInput, selectedDayNum, selectedDay, year
+    if userInput == 'q':
+        if curPrompt != "initial":
+            curPrompt = "initial"
+            return
+        else:
+            print("Goodbye!")
+            sys.exit(0)
+
+    if curPrompt == "initial":
+        if userInput == 'c':
+            curPrompt = "createDay"
+            return
+        try: numInput = int(userInput)
+        except ValueError:
+            pass
+        else:
+            selectedDayNum = numInput
+            selectedDay = importDay(selectedDayNum)
+            if selectedDay is None:
+                getInput("Day not found. Enter (C) to create it now, or anything else to continue.")
+                if userInput == 'c':
+                    userInput = selectedDayNum
+                    curPrompt = "createDay"
+                    process_input()
+                    return
+                curPrompt = "initial"
+                return
+            else:
+                curPrompt = "dayMenu"
+                return
+
+
+    if curPrompt == "createDay":
+        try: numInput = int(userInput)
+        except ValueError:
+            pass
+        else:
+            createDay(numInput)
+            print(f"Day {numInput} created.")
+            curPrompt = "initial"
+            return
+
+    if curPrompt == "dayMenu":
+        if userInput == 'e':
+            selectedDay = importDay(selectedDayNum)
+            print(f"Day {selectedDayNum} reloaded.")
+            return
+        if userInput == 'r':
+            path = f"inputs/aoc{year}-{selectedDayNum}-input.txt"
+            runDay(selectedDay, path, path,"Input")
+            return
+        if userInput == 's':
+            path = f"inputs/scratch-input.txt"
+            runDay(selectedDay, path, path, "Scratch input")
+            return
+        if userInput == 't':
+            path1 = f"inputs/aoc{year}-{selectedDayNum}-test1.txt"
+            path2 = f"inputs/aoc{year}-{selectedDayNum}-test2.txt"
+            runDay(selectedDay, path1, path2, "Test")
+            return
+    print("Input not recognized.")
+
+
+year = 2023
+selectedDayNum = -1
+selectedDay = None
+curPrompt = "initial"
+userInput = ""
 def main():
-    year = 2023
-    # pressing enter without entering any input for the first prompt will automatically select quickDay and run it on
-    # the test-input.txt file
-    # if quickRun is true on program start, DayManager automatically selects quickDay and runs
-    # the test-input.txt with no user input
-    quickDay = 4
-    quickRun = False
-    skipTest = False
+    global userInput
+    running = True
+    while running:
+        match curPrompt:
+            case "initial":
+                getInput("Enter a number (#) to select a day, or (C) to create a day. Enter (Q) anywhere else to return to this prompt. Enter (Q) here to quit.")
+            case "createDay":
+                getInput("Enter the day number (#)")
+            case "dayMenu":
+                getInput(f"Day {selectedDayNum}. Enter (R) to run on input data, (T) to run on test input data, or (S) to run on scratch file data. Enter (E) to reload the current day code.")
+            case _:
+                print("Not recognized.")
+        process_input()
 
-    day = None
-    dayNum = -1
-    while not day:
-        if quickRun or quickDay > -1:
-            userInput = quickDay
-        else:
-            userInput = input("Enter a number (#) to import that day, or (C) to create a new day: ").strip().lower()
-        if not userInput:
-            userInput = quickDay
-            quickRun = True
-        if userInput == "c":
-            dayNum = int(input("Enter the day number (#): "))
-            with open(f"aoc{year}-{dayNum}.py", "x") as f:
-                templateData = ""
-                with open(f"template.py") as template:
-                    templateData = template.read()
-                templateData = templateData.replace("{yearday}", f"{year}-{dayNum}")
-                f.write(templateData)
-            with open(f"inputs/aoc{year}-{dayNum}-input.txt", "x") as f:
-                f.write("")
-                f.close()
-            return 0
-        elif userInput == "q":
-            return 0
-        else:
-            dayNum = int(userInput)
-            day = imp.util.find_spec(f"aoc{year}-{dayNum}")
-            if not day:
-                if not quickRun:
-                    print("Day not found")
-                else:
-                    print("Quick run selected with no valid day, please enter day manually")
-                    quickRun = False
-    day = imp.import_module(f"aoc{year}-{dayNum}")
+def createDay(dayNum):
+    with open(f"aoc{year}-{dayNum}.py", "x") as f:
+        templateData = ""
+        with open(f"template.py") as template:
+            templateData = template.read()
+        templateData = templateData.replace("{yearday}", f"{year}-{dayNum}")
+        f.write(templateData)
+    with open(f"inputs/aoc{year}-{dayNum}-input.txt", "x") as f:
+        f.write("")
+    with open(f"inputs/aoc{year}-{dayNum}-test1.txt", "x") as f:
+        f.write("")
+    with open(f"inputs/aoc{year}-{dayNum}-test2.txt", "x") as f:
+        f.write("")
 
-    while True:
-        if quickRun:
-            userInput = "t" if not skipTest else "r"
-            quickRun = False
-        else:
-            userInput = input(f"Day {dayNum}, enter (R) to run on input data, or (T) to run on test file data, (Q) to quit: ").strip().lower()
+def importDay(dayNum):
+    path = f"aoc{year}-{dayNum}"
+    day = importlib.util.find_spec(path)
+    if not day:
+        return None
+    day = importlib.import_module(path)
+    return day
 
-        if not userInput:
-            userInput = "t"
-        if userInput == "r":
-            with open(f"inputs/aoc{year}-{dayNum}-input.txt") as f:
-                print("Input results:")
-                print(f"  Part 1: {day.part1(f)}")
-                f.seek(0)
-                print(f"  Part 2: {day.part2(f)}")
-        elif userInput == "t":
-            with open(f"inputs/test-input.txt") as f:
-                print("Test results:")
-                print(f"  Part 1: {day.part1(f)}")
-                f.seek(0)
-                print(f"  Part 2: {day.part2(f)}")
-        elif userInput == "q":
-            return 0
-
+def runDay(day, filePath1, filePath2, resultTitle):
+    print(f"{resultTitle} results:")
+    with open(filePath1) as f:
+        print(f"  Part 1: {day.part1(f)}")
+    with open(filePath2) as f:
+        print(f"  Part 2: {day.part2(f)}")
 
 if __name__ == "__main__":
     main()
